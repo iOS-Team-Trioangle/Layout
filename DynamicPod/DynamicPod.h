5b0c40a98401ecef9bc8c6ab3b729ead87464da6
Kiran <ios@trioangle.com>
2022-02-14 16:49:41 +0530

    File

---
+++ DynamicPod/DynamicPod.h
@@ -0,0 +1,18 @@
+//
+//  DynamicPod.h
+//  DynamicPod
+//
+//  Created by trioangle on 14/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DynamicPod.
+FOUNDATION_EXPORT double DynamicPodVersionNumber;
+
+//! Project version string for DynamicPod.
+FOUNDATION_EXPORT const unsigned char DynamicPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicPod/PublicHeader.h>
+
+
